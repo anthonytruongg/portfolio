14875aa59bb082336f0646a22fd5ef3c6623c21b
anthonytruongg <anthonytruongg1@gmail.com>
2023-01-10 08:46:50 -1000

    Initial commit

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,21 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Russo+One"
+        rel="stylesheet"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Heebo"
+        rel="stylesheet"
+      />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
